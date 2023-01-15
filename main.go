1978d818e90454a05475daa49261134518994cee
sadcomm <poebot3r@gmail.com>
2023-01-15 22:42:10 +0800

    update ws routes

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	ws.StartServer();
+	ws.SetupRoutes();
 }
--- ws/ws.go
+++ ws/ws.go
@@ -3,7 +3,6 @@ package ws
 import (
     "log"
     "net/http"
-    "strings"
 
     "github.com/gorilla/websocket"
 )
@@ -13,75 +12,39 @@ var upgrader = websocket.Upgrader{
         return true 
     },
 }
-var todoList []string
 
-func getCmd(input string) string {
-    inputArr := strings.Split(input, " ")
-    return inputArr[0]
-}
 
-func getMessage(input string) string {
-    inputArr := strings.Split(input, " ")
-    var result string
-    for i := 1; i < len(inputArr); i++ {
-        result += inputArr[i]
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+    // Upgrade upgrades the HTTP server connection to the WebSocket protocol.
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Print("upgrade failed: ", err)
+        return
     }
-    return result
-}
+    defer conn.Close()
 
-func updateTodoList(input string) {
-    tmpList := todoList
-    todoList = []string{}
-    for _, val := range tmpList {
-        if val == input {
-            continue
-        }
-        todoList = append(todoList, val)
-    }
-}
-
-func StartServer() {
-    http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
-        // Upgrade upgrades the HTTP server connection to the WebSocket protocol.
-        conn, err := upgrader.Upgrade(w, r, nil)
+    // Continuosly read and write message
+    for {
+        mt, message, err := conn.ReadMessage()
         if err != nil {
-            log.Print("upgrade failed: ", err)
-            return
+            log.Println("read failed:", err)
+            break
         }
-        defer conn.Close()
-
-        // Continuosly read and write message
-        for {
-            mt, message, err := conn.ReadMessage()
-            if err != nil {
-                log.Println("read failed:", err)
-                break
-            }
-            input := string(message)
-            cmd := getCmd(input)
-            msg := getMessage(input)
-            if cmd == "add" {
-                todoList = append(todoList, msg)
-            } else if cmd == "done" {
-                updateTodoList(msg)
-            }
-            output := "Current Todos: \n"
-            for _, todo := range todoList {
-                output += "\n - " + todo + "\n"
-            }
-            output += "\n----------------------------------------"
-            message = []byte(output)
-            err = conn.WriteMessage(mt, message)
-            if err != nil {
-                log.Println("write failed:", err)
-                break
-            }
+        err = conn.WriteMessage(mt, message)
+        if err != nil {
+            log.Println("write failed:", err)
+            break
         }
-    })
+    }
+}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "./ui/test/websockets.html")
-    })
+func testingPage(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./ui/test/websockets.html")
+}
+
+func SetupRoutes() {
+    http.HandleFunc("/", testingPage);
+    http.HandleFunc("/todo", wsEndpoint);
 
     http.ListenAndServe(":8080", nil)
 }
